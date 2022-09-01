d18a787d21909f5d69cb2eaf115779245620a7fc
Datasciensyash <gribulae@gmail.com>
2022-09-01 13:17:01 +0300

    fix: Fix requirements.txt (win32gui -> pywin32) and add run script to entry_points as run_deepfish_bot

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ from setuptools import find_packages, setup
 THIS_DIR = Path(__file__).parent
 
 
-def _load_requirements(path_dir: Path, comment_char: str = "#"):
+def _load_requirements(path_dir: Path):
     requirements_directory = path_dir / "requirements.txt"
     requirements = []
     with requirements_directory.open("r") as file:
         for line in file.readlines():
-            line = line.lstrip()
-            # Filter all comments
-            if comment_char in line:
-                line = line[: line.index(comment_char)]
-            if line:  # If requirement is not empty
-                requirements.append(line)
+            requirements.append(line.lstrip())
     return requirements
 
 
@@ -24,4 +19,9 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=_load_requirements(THIS_DIR),
+    entry_points={
+        "console_scripts": [
+            "run_deepfish_bot = deepfish.scripts.run:main",
+        ],
+    },
 )
